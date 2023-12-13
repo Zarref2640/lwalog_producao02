2e2a4c618869280e450380bccb3268cebff15e07
Zarref2640 <153675248+Zarref2640@users.noreply.github.com>
2023-12-13 12:29:50 -0300

    Commit Versao_02

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,  render_template
+
+app = Flask(__name__)
+# route -> nome_da_pagina.com/
+# função -> o que voce quer exibir naquela pagina
+# template
+
+@app.route("/")
+def homepage():
+        return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+        return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuarios>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
+if __name__=="__main__":
+    app.run(debug=True)
